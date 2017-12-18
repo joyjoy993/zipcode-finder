15ff064b465a678f6626540aa2482ce7b3f0c10e
joy <jg4743@nyu.edu>
2017-12-18 18:17:36 -0500

    Add echo when finishing initialization

--- db-seed/init.sh
+++ db-seed/init.sh
@@ -5,3 +5,5 @@ sleep 5
 gunzip /seed/init.json.gz && mongoimport --host mymongo:27017 --db zipcode_finder_development --collection zipcodes --type json --file /seed/init.json --jsonArray
 
 mongo mymongo:27017/zipcode_finder_development /seed/init.js
+
+echo "database initialization finished..."
