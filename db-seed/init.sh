3d72b75eea52de17bf66f19a603a19d3f22f75ca
joy <jg4743@nyu.edu>
2017-12-18 18:16:19 -0500

    Use shell to initialize seed

---
+++ db-seed/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sleep 5
+
+gunzip /seed/init.json.gz && mongoimport --host mymongo:27017 --db zipcode_finder_development --collection zipcodes --type json --file /seed/init.json --jsonArray
+
+mongo mymongo:27017/zipcode_finder_development /seed/init.js
